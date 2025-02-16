4373163e806c4e30157025eceb02baed9610252d
erikjwright <hey@erikwright.dev>
2025-02-16 19:10:14 +0100

    add darwin direnv

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,34 +1,41 @@
 { pkgs, neovim-nightly-overlay, ... }:
 {
-  home.username = "erik";
-  home.homeDirectory = "/Users/erik";
+	home.username = "erik";
+	home.homeDirectory = "/Users/erik";
 
-  home.stateVersion = "24.11";
+	home.stateVersion = "24.11";
 
-  # xdg.enable = true;
+	# xdg.enable = true;
 
-  home.packages = with pkgs; [
-    appflowy
-    chezmoi
-    dbeaver-bin
-    direnv
-    eza
-    fzf
-    git
-    # ghostty
-    lazygit
-    neovim-nightly-overlay.packages.${system}.default
-    nixfmt-rfc-style
-    ripgrep
-    starship
-    thunderbird-latest-unwrapped
-    yazi
-    zoxide
-  ];
+	home.packages = with pkgs; [
+		appflowy
+		chezmoi
+		dbeaver-bin
+		direnv
+		eza
+		fzf
+		git
+		# ghostty
+		lazygit
+		neovim-nightly-overlay.packages.${system}.default
+		nixfmt-rfc-style
+		ripgrep
+		starship
+		thunderbird-latest-unwrapped
+		yazi
+		zoxide
+	];
 
-  programs.home-manager.enable = true;
-  # programs.direnv.enable = true;
-  # programs.htop.enable = true;
-  # programs.htop.settings.show_program_path = true;
+	programs = {
+		home-manager.enable = true;
+		direnv = {
+			enable = true;
+			enableZshIntegration = true;
+			nix-direnv.enable = true;
+		};
+	}
+		# programs.direnv.enable = true;
+		# programs.htop.enable = true;
+		# programs.htop.settings.show_program_path = true;
 
 }
